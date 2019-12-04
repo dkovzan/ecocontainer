4e940a290a080b3b99d70356912d8fcfc83622f6
Denis Kovzan <32581231+dkovzan@users.noreply.github.com>
2019-12-04 20:15:05 +0300

    App structure is added

---
+++ src/main/java/com/group/ecocontainer/EcocontainerApplication.java
@@ -0,0 +1,14 @@
+package com.group.ecocontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class EcocontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocontainerApplication.class, args);
+	}
+
+}
